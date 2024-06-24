cee3f5fa8d31ec2dd0a22f8c5b4c4ed5193cd003
ABT Aurelien <aurelien@abrassart.net>
2024-06-24 10:15:05 +0200

    Ajoutez des fichiers projet.

---
+++ Week4_Q_Heritage/Animal.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Week4_Q_Heritage
+{
+    public abstract class Animal
+    {
+        protected string _name;
+        protected int _legsCount;
+        protected bool _hairy;
+
+        public Animal(string name, int legsCount, bool hairy)
+        {
+            _name = name;
+            _legsCount = legsCount;
+            _hairy = hairy;
+        }
+
+        public virtual void Move()
+        {
+            Console.WriteLine($"{_name} se déplace...");
+            Console.WriteLine(_hairy ? "...avec les poils au vent." : "vec la peau nue.");
+
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{_name} mange");
+        }
+
+    }
+
+    public class Biped : Animal
+    {
+        public Biped(string name) : base (name, 2, true)
+        {
+
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{_name} se déplace à {_legsCount} jambes car il est bipède");
+        }
+
+    }
+
+    public class Quadriped : Animal
+    {
+        public Quadriped(string name) : base(name, 4, true)
+        {
+
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{_name} se déplace à {_legsCount} jambes car il est quadripède");
+        }
+
+    }
+}
---
+++ Week4_Q_Heritage/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Week4_Q_Heritage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Biped homme = new Biped("Bob");
+            Biped singe = new Biped("Abu");
+
+            homme.Eat();
+            homme.Move();
+            singe.Eat();
+            singe.Move();
+
+            Quadriped elephant = new Quadriped("Babar");
+            Quadriped tortue = new Quadriped("Franklin");
+            Quadriped lézard = new Quadriped("Oscar");
+
+            elephant.Eat();
+            elephant.Move();
+            tortue.Eat();
+            tortue.Move();
+            lézard.Eat();
+            lézard.Move();
+
+        }
+    }
+}
